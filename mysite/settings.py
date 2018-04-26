62ef51cfbae8fcd6678dad9cba35337304385c04
Niklas Schünemann <niklas.schuenemann@stud.hs-hannover.de>
2018-04-26 16:40:33 +0200

    fixed issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 from backports import configparser
 
 config = configparser.ConfigParser()
-config.read('secrets.ini')
+config.read('/home/ubuntu/eduDSExample/secrets.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,7 @@ SECRET_KEY = config['DB']['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
