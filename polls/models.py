3598ba1c85aa8189b5590a281a879baca1fbf655
Torben <torben.tietze@googlemail.com>
2018-05-31 11:15:12 +0200

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,7 @@ from django.utils.encoding import python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    os_type = sys.platform
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
